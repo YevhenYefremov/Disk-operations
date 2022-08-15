df28fd9e14f705645e3f549de271ab33c4a2caab
root <YevhenYefremov>
2022-08-15 12:42:57 +0300

    Free HDD space checking

--- Free-space-on-hdd.py
+++ Free-space-on-hdd.py
@@ -26,6 +26,20 @@ def get_free_space_mb(folder):
         return st.f_bavail * st.f_frsize/1024/1024
 
 
+def get_hdd_spaces_mb(folder):
+    if platform.system() == 'Windows':
+        total_bytes = ctypes.c_ulonglong(0)
+        free_bytes = ctypes.c_ulonglong(0)
+        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(folder), None, ctypes.pointer(total_bytes),
+                                                   ctypes.pointer(free_bytes))
+        return free_bytes.value/1024/1024/1024, total_bytes.value/1024/1024/1024
+    else:
+        st = os.statvfs(folder)
+        return st.f_bavail * st.f_frsize/1024/1024, st.f_blocks * st.f_frsize/1024/1024 * st.f_ffree
+
+
 if __name__ == '__main__':
     for drive in get_drives():
-        print(drive, ': ', get_free_space_mb(f'{drive}:\\').__round__(1), 'GB')
+        free, total = get_hdd_spaces_mb(f'{drive}:\\')
+        print(f'{drive}: {free.__round__(1)} GB / {total.__round__(1)} GB')
+        # print(drive, ': ', get_free_space_mb(f'{drive}:\\').__round__(1), 'GB')
