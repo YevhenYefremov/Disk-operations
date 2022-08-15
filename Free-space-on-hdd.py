45d8cc7a1581607be17eca6f90383ccd44e256f4
root <YevhenYefremov>
2022-08-15 16:32:01 +0300

    Free HDD space checking

--- Free-space-on-hdd.py
+++ Free-space-on-hdd.py
@@ -1,8 +1,32 @@
 import ctypes
 import os
+import sys
 import platform
 import string
 from ctypes import windll
+import json
+import logging
+import filecmp
+from shutil import copyfile
+
+filename = 'Server 40 HDD'
+dstpath = '\\\\192.168.11.107\\test\\'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s',
+                              '%m-%d-%Y %H:%M:%S')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler(f'{filename}.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
 
 
 def get_drives():
@@ -14,16 +38,6 @@ def get_drives():
         bitmask >>= 1
 
     return drives
-
-
-def get_free_space_mb(folder):
-    if platform.system() == 'Windows':
-        free_bytes = ctypes.c_ulonglong(0)
-        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(folder), None, None, ctypes.pointer(free_bytes))
-        return free_bytes.value/1024/1024/1024
-    else:
-        st = os.statvfs(folder)
-        return st.f_bavail * st.f_frsize/1024/1024
 
 
 def get_hdd_spaces_mb(folder):
@@ -39,7 +53,23 @@ def get_hdd_spaces_mb(folder):
 
 
 if __name__ == '__main__':
+    data = [{'Drive': ('free', 'total')}]
     for drive in get_drives():
         free, total = get_hdd_spaces_mb(f'{drive}:\\')
-        print(f'{drive}: {free.__round__(1)} GB / {total.__round__(1)} GB')
-        # print(drive, ': ', get_free_space_mb(f'{drive}:\\').__round__(1), 'GB')
+        data[0][drive] = (free, total)
+    try:
+        with open(f'{dstpath}{filename}.json', 'w') as outfile:
+            json.dump(data, outfile)
+            if os.path.exists(f'{filename}.log'):
+                if os.path.exists(f'{dstpath}{filename}.log'):
+                    if not filecmp.cmp(f'{filename}.log', f'{dstpath}{filename}.log'):
+                        copyfile(rf'{filename}.log', rf'{dstpath}{filename}.log')
+                else:
+                    copyfile(rf'{filename}.log', rf'{dstpath}{filename}.log')
+
+    except FileNotFoundError:
+        logger.error('FileNotFoundError: No such file or directory')
+        logger.info(data)
+    except Exception as e:
+        logger.error(e.with_traceback)
+        logger.info(data)
